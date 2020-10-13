65706cca0046493fc050280391d3078cfa8da0a5
Yurii Makukha <yura@consulting.mk>
2020-10-13 15:39:44 +0300

    change country related data

--- config.dist.js
+++ config.dist.js
@@ -8,7 +8,7 @@ module.exports = {
 		port: 9200,
 		log: ['info', 'error']
 	},
-	data: { // absolute paths to the data folders (see https://github.com/opentender-ke/opentender-data)
+	data: { // absolute paths to the data folders (see https://github.com/opentender-ke-health/opentender-data)
 		shared: '/var/www/opentender/data/shared',
 		path: '/var/www/opentender/data/backend',
 		tenderapi: '/var/www/opentender/data/tenderapi'
